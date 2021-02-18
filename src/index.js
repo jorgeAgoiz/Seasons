e5d28b8b0cedb0b2c0a6ad2cbcb4956d66f22aca
jorgeAgoiz <jorgeagoiz@gmail.com>
2021-02-18 21:22:29 +0100

    Learning about Components
    Functional component or Class component.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// *******************************************************************
+// Functional Component Example Counter
+// const Contador = () => {
+//   let [contador, setContador] = useState(51);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//   return (
+//     <div>
+//       <span>{contador}</span>
+//       <button
+//         onClick={() => {
+//           setContador(contador + 1);
+//         }}
+//       >
+//         Incrementar
+//       </button>
+//       <button
+//         onClick={() => {
+//           setContador(contador - 1);
+//         }}
+//       >
+//         Decrementar
+//       </button>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Contador />
+//     </div>
+//   );
+// };
+// *******************************************************************
+
+// ******************************** Example with class component *******************************
+// Class Component Example
+class App extends React.Component {
+  // To initialize the state in constructor
+  constructor(props) {
+    super(props);
+    // First instance state object with null properties
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //When the location load, use setState method to update lat property
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err),
+    );
+  }
+
+  // Render method
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
